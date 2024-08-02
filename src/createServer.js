0a0a194b44724b1ced6e0ad4ed5fe3123cc4aa55
Jenish <zinzuvadiajenish@gmail.com>
2024-08-02 12:28:54 +0530

    Added ubuntu & mac Addressing to console

--- src/createServer.js
+++ src/createServer.js
@@ -71,7 +71,7 @@ export async function createServer() {
   function getIPv4Address() {
     const interfaces = os.networkInterfaces();
     for (const name of Object.keys(interfaces)) {
-        if (name.includes('Wi-Fi') || name.includes('Wireless')) {
+        if (name.includes('Wi-Fi') || name.includes('Wireless') || name.includes('wlp2s0') || name.startsWith('en') || name.startsWith('eth')) {
             for (const iface of interfaces[name]) {
                 if (iface.family === 'IPv4' && !iface.internal) {
                     return iface.address;
