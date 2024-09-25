9af208bdf0efe29d25692c42b6c94c23da0efb81
prem-credsoft <premcredsoft@gmail.com>
2024-09-25 13:30:11 +0530

    Updated server.js & generateToken.js

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-import express from 'express';
-import chalk from 'chalk';
 import { createServer } from './src/createServer.js';
 import dotenv from 'dotenv';
 
--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-export const generateToken = (payload, expiresIn = '1h') => {
+export const generateToken = (payload, expiresIn = '2d') => {
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
 };
 
